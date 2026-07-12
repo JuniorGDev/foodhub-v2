f74aa47b68ffb71cc222a1c167769e3b7e56e07b
Junior G <juniorgeo70@hotmail.com>
2026-07-12 14:51:16 -0300

    feat: add new constants about user type

---
+++ src/main/java/br/com/foodhub/shared/constants/UserTypeConstants.java
@@ -0,0 +1,10 @@
+package br.com.foodhub.shared.constants;
+
+public class UserTypeConstants {
+
+    private UserTypeConstants() {}
+
+    public static final String OWNER = "OWNER";
+    public static final String ADMIN = "ADMIN";
+    public static final String USER = "USER";
+}
